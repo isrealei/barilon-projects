1269ad6da4ad78a0b25779b8438f1ede4a8a583c
isreal <isrealurephu@gmail.com>
2025-09-30 19:44:40 +0100

    saved

--- Stage-1/scripts/result.sh
+++ Stage-1/scripts/result.sh
@@ -32,6 +32,32 @@ systemctl start docker
 # Add ubuntu user to docker group
 usermod -aG docker ubuntu
 
+#install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+unzip awscliv2.zip
+sudo ./aws/install
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+#Pull params from SSM
+
+REDIS_HOST=$(aws ssm get-parameters --region us-east-1 --names REDIS_HOST --query Parameters[0].Value)
+REDIS_HOST=$(echo $REDIS_HOST | tr -d '"')
+
+POSTGRES_USER=$(aws ssm get-parameters --region us-east-1 --names POSTGRES_USER --query Parameters[0].Value)
+POSTGRES_USER=$(echo $POSTGRES_USER | tr -d '"')
+
+POSTGRES_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names POSTGRES_PASSWORD --query Parameters[0].Value)
+POSTGRES_PASSWORD=$(echo $POSTGRES_PASSWORD | tr -d '"')
+
+POSTGRES_DB=$(aws ssm get-parameters --region us-east-1 --names POSTGRES_DB --query Parameters[0].Value)
+POSTGRES_DB=$(echo $POSTGRES_DB | tr -d '"')
+
+POSTGRES_HOST=$(aws ssm get-parameters --region us-east-1 --names POSTGRES_HOST --query Parameters[0].Value)
+POSTGRES_HOST=$(echo $POSTGRES_HOST | tr -d '"')
+
+
 # Clone repo (HTTPS is safer for automation)
 git clone https://github.com/isrealei/barilon-projects.git
 
@@ -39,6 +65,10 @@ cd barilon-projects/Stage-1/result
 
 # Create or overwrite .env file with REDIS_HOST
 echo "REDIS_HOST=${REDIS_HOST}" > .env
+echo "POSTGRES_USER=${POSTGRES_USER}" >> .env
+echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> .env
+echo "POSTGRES_DB=${POSTGRES_DB}" >> .env
+echo "POSTGRES_HOST=${POSTGRES_HOST}" >> .env   
 
 # Build and run container
 docker compose up -d --build
